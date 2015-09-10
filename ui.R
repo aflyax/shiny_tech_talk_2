953a3e4715d7707d16e2191b5d3a1d5905d27fb7
Alexander Flyax <aflyax@ch-nb-725.local>
2015-09-09 22:38:42 -0500

    STEP 3 (you): create selectizeInput for y; allow only one var to be selected; choose depth by default

--- ui.R
+++ ui.R
@@ -2,31 +2,46 @@ shinyUI(fluidPage(
   headerPanel(
     'Shiny Tech Talk'
   ),
-  sidebarPanel('sidebar'
+  sidebarPanel('sidebar',
+               selectizeInput(inputId = 'select_X',
+                              label = 'please select X',
+                              choices = names(dataset),
+                              select = names(dataset)[1:2],
+                              multiple = TRUE,
+                              options = list(
+                                plugins = list('remove_button', 'drag_drop')
+                              )
+               )
+               
+               , selectizeInput(inputId = 'select_y',
+                                label = 'please select y',
+                                select = 'depth',
+                                choices = names(dataset),
+                                multiple = FALSE)
   ),
   
   mainPanel('mainbar',
-    tabsetPanel(
-      tabPanel(inputId = 'raw_data',
-               title = 'raw data'
-      ),
-      
-      tabPanel(inputId = 'selected_data',
-               title = 'selected data'
-      ),
-      
-      tabPanel(inputId = 'data_plot',
-               title = 'plot data'
-      ),
-      
-      tabPanel(inputId = 'model_output',
-               title = 'model output'
-      ),
-      
-      tabPanel(inputId = 'model_plot',
-               title = 'model plot'
-      )
-    )
+            tabsetPanel(
+              tabPanel(inputId = 'raw_data',
+                       title = 'raw data'
+              ),
+              
+              tabPanel(inputId = 'selected_data',
+                       title = 'selected data'
+              ),
+              
+              tabPanel(inputId = 'data_plot',
+                       title = 'plot data'
+              ),
+              
+              tabPanel(inputId = 'model_output',
+                       title = 'model output'
+              ),
+              
+              tabPanel(inputId = 'model_plot',
+                       title = 'model plot'
+              )
+            )
   )
   
 )
