6681b4b9af65f6e4c0cd6c3e0aabf5e41c06861f
Alexander Flyax <aflyax@ch-nb-725.local>
2015-09-09 22:25:07 -0500

    STEP 1: generate additional tabs called 'selected_data', 'data_plot', 'model_output', 'model_plot'

--- ui.R
+++ ui.R
@@ -9,6 +9,22 @@ shinyUI(fluidPage(
     tabsetPanel(
       tabPanel(inputId = 'raw_data',
                title = 'raw data'
+      ),
+      
+      tabPanel(inputId = 'selected_data',
+               title = 'selected data'
+      ),
+      
+      tabPanel(inputId = 'data_plot',
+               title = 'plot data'
+      ),
+      
+      tabPanel(inputId = 'model_output',
+               title = 'model output'
+      ),
+      
+      tabPanel(inputId = 'model_plot',
+               title = 'model plot'
       )
     )
   )
